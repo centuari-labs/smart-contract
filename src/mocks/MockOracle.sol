4a093ba573a724cf14e8d1dc4c3346952657fa9d
WillGD <56427283+willgd-git@users.noreply.github.com>
2025-04-03 12:15:58 +0700

    feat: added mock oracle interface to give better accessability

--- src/mocks/MockOracle.sol
+++ src/mocks/MockOracle.sol
@@ -2,13 +2,12 @@
 pragma solidity ^0.8.13;
 
 import {Ownable} from "openzeppelin-contracts/contracts/access/Ownable.sol";
+import {IMockOracle} from "../interfaces/IMockOracle.sol";
 
-contract MockOracle is Ownable {
+contract MockOracle is Ownable, IMockOracle {
     address public baseFeed;
     address public quoteFeed;
     uint256 public price;
-
-    error InvalidPrice();
 
     constructor(address baseFeed_, address quoteFeed_) Ownable(msg.sender) {
         baseFeed = baseFeed_;
