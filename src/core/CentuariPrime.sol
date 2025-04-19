e88de9f31346fd4a558cae473f67ffdcb46b187f
willgd <56427283+willgd-git@users.noreply.github.com>
2025-04-19 22:14:28 +0700

    hotfix: bug fix on centuari prime - reallocate function

--- src/core/CentuariPrime.sol
+++ src/core/CentuariPrime.sol
@@ -421,7 +421,7 @@ contract CentuariPrime is Ownable, ReentrancyGuard {
         emit CentuariPrimeEventsLib.SetWithdrawQueue(msg.sender, address(vault), withdrawQueue);
     }
 
-    function reallocate() external onlyActiveVault(config.id()) onlyVaultOwner(config.id()) {
+    function reallocate() external {
         //@todo implement reallocate
     }
     
